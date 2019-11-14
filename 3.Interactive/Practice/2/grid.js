30036fd4ab810500990a282783779d48e2bdccd1
EC2 Default User <ec2-user@ip-172-31-31-208.ec2.internal>
2019-11-14 20:43:55 +0000

    commit

--- 3.Interactive/Practice/2/grid.js
+++
@@ -1,9 +0,0 @@
-
-// async function dataLoad() {
-//   const data = await d3.json("json/portrait.json");
-//   console.log(data);
-// }
-
-d3.json("json/portrait.json", function(data) {
-    console.log(data);
-});
